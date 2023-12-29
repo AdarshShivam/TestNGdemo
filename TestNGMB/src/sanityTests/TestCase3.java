7f0ceed5e34858e2638d120a17e0662590688713
AdarshShivam <adarsh.shivam004@gmail.com>
2023-12-29 12:02:34 +0530

    First Change

---
+++ TestNGMB/src/sanityTests/TestCase3.java
@@ -0,0 +1,16 @@
+package sanityTests;
+import org.testng.annotations.Test;
+
+public class TestCase3 {
+
+	@Test
+	public void m5() {
+	System.out.println("This is m5 from TestCase3");	
+	}
+	 
+	@Test
+	public void m6() {
+		System.out.println("This is m6 from TestCase3");
+	}
+	
+}
